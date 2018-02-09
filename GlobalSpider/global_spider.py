26274dc1241dfeef5ae65e76b66c29b3f2a8545d
myirelias <myirelia@aliyun.com>
2018-02-09 18:47:42 +0800

    new global

---
+++ GlobalSpider/global_spider.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python
+# coding=UTF-8
+
+from lxml import etree
+
+
+class Spider(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def spider_content_data(**kwargs):
+        """
+        解析content中指定内容，必须传入 content 和 xpather
+        :param kwargs: content 为需要解析的页面content, xpather 为xpath解析规则
+        :return: 解析后的内容，若xpather为dict类型则返回dict类型，其余返回list
+        """
+
+        if 'content' not in kwargs or 'xpather' not in kwargs:
+            print('must require enough args')
+            return
+
+        xpather = kwargs['xpather']
+        content = kwargs['content']
+
+        try:
+            selector = etree.HTML(content)
+        except:
+            selector = content  # 针对需要解析多次的页面采取的方式，前面几次返回的是 etree的element对象，继续解析
+
+        if isinstance(kwargs['xpather'], dict):  # 解析规则以dict的格式传过来的，这种规则默认解析结果为一条数据
+            response = {}
+            for eachkey in xpather:
+                res = selector.xpath(xpather[eachkey])
+                if eachkey == 'img':
+                    response[eachkey] = \
+                        ','.join(res).replace('\n', '').replace('\r', '').replace(' ', '').replace('\u3000', '')
+                else:
+                    response[eachkey] = ''.join(res).replace('\n', '').replace('\r', '').replace(' ', '').\
+                        replace('\u3000', '').replace('\t', '')
+        elif isinstance(kwargs['xpather'], str):  # 解析规则为str
+            response = selector.xpath(kwargs['xpather'])
+        else:
+            print('xpather must be dict or string')
+            return
+
+        return response
