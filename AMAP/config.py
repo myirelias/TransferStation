52a63ecb9cd87a9fafec555e6fe900840971d95d
myirelias <myirelia@aliyun.com>
2018-06-01 14:26:42 +0800

    modify output result from json to xml

--- AMAP/config.py
+++ AMAP/config.py
@@ -18,24 +18,56 @@ HEADERS_COMMENTS = {
 
 }
 
-
 # 请求参数
+# city=510104&citylimit=true&extensions=all&key=19bb8490a86b16e901a701178d6ed6aa&keywords=&offset=25&page=1&types=120300
 PARAMS = {
     'keywords': '',  # 查询关键字
     'types': '120302',  # 查询POI类型
     'city': '',  # 查询城市
     'citylimit': 'true',  # 仅返回指定城市数据
-    'offset': 20,  # 每页记录数据
+    'offset': 25,  # 每页记录数据
     'page': 1,  # 当前页数
     'extensions': 'all',  # 返回结果控制
+    'output': 'xml'  # 千万不能用json，数据是乱的，名称地址无法一一对应，坑
 }
+
 # xpath
-XPATH_CITYLIST_A = ".//*[@class='mp-city-list']/descendant::a"
-XPATH_TEXT = ".//text()"
-XPATH_HREF = ".//@href"
-XPATH_TOURIST_A = ".//*/a[@class='name']"
-XPATH_TOURIST_DETAIL = {
-    '暂无指定字段'
+XPATH_POIS = ".//*/pois[@type='list']/poi"
+XPATH_DETAIL = {
+    "id": ".//id/text()",
+    "name": ".//name/text()",
+    "pcode": ".//pcode/text()",
+    "pname": ".//pname/text()",
+    "citycode": ".//citycode/text()",
+    "cityname": ".//cityname/text()",
+    "adcode": ".//adcode/text()",
+    "adname": ".//adname/text()",
+    "address": ".//address/text()",
+    "alias": ".//alias/text()",
+    "biz_ext": ".//biz_ext/text()",
+    "biz_type": ".//biz_type/text()",
+    "business_area": ".//business_area/text()",
+    "discount_num": ".//discount_num/text()",
+    "email": ".//email/text()",
+    "entr_location": ".//entr_location/text()",
+    "exit_location": ".//exit_location/text()",
+    "gridcode": ".//gridcode/text()",
+    "groupbuy_num": ".//groupbuy_num/text()",
+    "indoor_data": ".//indoor_data/text()",
+    "indoor_map": ".//indoor_map/text()",
+    "location": ".//location/text()",
+    "navi_poiid": ".//navi_poiid/text()",
+    "postcode": ".//postcode/text()",
+    "tag": ".//tag/text()",
+    "tel": ".//tel/text()",
+    "type": ".//type/text()",
+    "typecode": ".//typecode/text()",
+    "website": ".//website/text()"
+}
+XPATH_PHOTOS = ".//photo"
+XPATH_PHOTO_DETAIL = {
+    "title": ".//title/text()",
+    "url": ".//url/text()"
 }
 
 # file
